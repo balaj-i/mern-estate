facd592742ca69704f3efd04c50429422c1ae37c
balaj-i <balajiggp@gmail.com>
2025-02-14 16:42:00 +0530

    connecting to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MONGOdb')
+}).catch((err)=>{
+    console.log(err)
+})
+ 
 const app=express()
 
 app.listen(3000,()=>{
