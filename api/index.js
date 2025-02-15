ab255c620829ac16203be2ce5b9810c027e1ebd5
balaj-i <balajiggp@gmail.com>
2025-02-15 20:29:04 +0530

    create a middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,7 +12,22 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
  
 const app=express()
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
+})
+
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
+
+//middleware
+app.use((err,req,res,next)=>{
+    const statuscode=err.statuscode || 500;
+    const message=err.message || 'Internal Server Error';
+    return res.status(statuscode).json({
+        success:false,
+        statuscode,message
+
+    });
 })
